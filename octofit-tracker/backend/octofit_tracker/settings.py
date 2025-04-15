5798ad32a53f6485ee173796f4d391d03e9ba683
Wind <m110033@users.noreply.github.com>
2025-04-15 16:54:59 +0000

    Step 4 Finished

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-8^!pcsvmb^llu7k&jmw+!a$smtftrphsi7m%zyx10*sn5*dfa!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'special-space-lamp-x5j644jj67h6xp6-8000.app.github.dev']
 
 
 # Application definition
